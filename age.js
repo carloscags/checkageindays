cc3ab6a8bda5c14c05de8cfc8e0193dc3be107de
Carlos <carlosaugustocf99@gmail.com>
2021-02-04 19:16:26 -0300

    update code

---
+++ age.js
@@ -0,0 +1,23 @@
+
+
+
+
+checkAge = () => {
+    
+    const input = document.getElementById("checkAge")
+    
+    const age = parseInt(input.value)
+    
+    const greenting = age * 365
+    
+    const showAgeInDays = document.getElementById("response")
+
+    showAgeInDays.textContent = greenting
+
+    console.log(greenting)
+
+
+}
+
+
+
